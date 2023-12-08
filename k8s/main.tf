89482304e41cf31a2dc6376ea16f3d7a4acd1504
Antoine Latter <aslatter@gmail.com>
2023-12-08 16:02:09 -0600

    install k8s metrics-server

--- k8s/main.tf
+++ k8s/main.tf
@@ -104,3 +104,16 @@ resource "helm_release" "lb_controller" {
     }
   })]
 }
+
+// k8s metrics server
+resource "helm_release" "k8s_metrics" {
+  name = "k8s-metrics-server"
+
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.11.0"
+
+  namespace = "kube-system"
+
+  wait = false
+}
