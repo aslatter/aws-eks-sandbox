bb75d84c19b0406665829decf333c8f047efcc2d
Antoine Latter <aslatter@gmail.com>
2024-08-25 19:57:06 -0500

    naming wibble

--- eks/fargate.tf
+++ eks/fargate.tf
@@ -1,7 +1,7 @@
 
-resource "aws_eks_fargate_profile" "karpenter" {
+resource "aws_eks_fargate_profile" "main" {
   cluster_name           = aws_eks_cluster.main.name
-  fargate_profile_name   = "karpenter-${local.entropy}"
+  fargate_profile_name   = "main-${local.entropy}"
   pod_execution_role_arn = aws_iam_role.fargate_execution.arn
   subnet_ids             = aws_subnet.private[*].id
 
