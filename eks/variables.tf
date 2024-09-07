873318087e69bcd2a9007b8f2d3bd67765a2202f
Antoine Latter <aslatter@gmail.com>
2024-09-06 21:09:26 -0500

    proper IPv4 subnets
    The old subnet splits made more sense when we were using IPv6 for pod
    traffic.

--- eks/variables.tf
+++ eks/variables.tf
@@ -63,24 +63,22 @@ variable "vpc_cidr_block" {
 
 // TODO - secondary CIDR blocks ?
 
-// We've planned out the splits for 2 AZs, even though we're only
-// using one
-// https://www.davidc.net/sites/default/subnets/subnets.html?network=10.0.0.0&mask=16&division=21.ff4200
+// We've planned out the splits for 3 AZs, even though we're only
+// using one.
 
 variable "vpc_public_subnets" {
   type    = list(string)
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
+  default = ["10.0.96.0/22", "10.0.100.0/22", "10.0.104.0/22"]
 }
 
 variable "vpc_private_subnets" {
   type    = list(string)
-  default = ["10.0.4.0/23", "10.0.6.0/23"]
+  default = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
 }
-
 
 variable "vpc_intra_subnets" {
   type    = list(string)
-  default = ["10.0.2.0/24", "10.0.3.0/24"]
+  default = ["10.0.108.0/22", "10.0.112.0/22", "10.0.116.0/22"]
 }
 
 variable "public_access_cidrs" {
