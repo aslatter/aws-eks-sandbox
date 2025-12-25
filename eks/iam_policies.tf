be9bd2725f6b5a3e805c43fdb045b00e85d88576
Antoine Latter <aslatter@gmail.com>
2025-12-25 07:37:42 -0600

    grant iam:ListInstanceProfiles to Karpenter
    It isn't actually required but it cleans-up the Karpenter
    error-logs.

--- eks/iam_policies.tf
+++ eks/iam_policies.tf
@@ -232,7 +232,7 @@ data "aws_iam_policy_document" "karpenter" {
   statement {
     // AllowInstanceProfileReadActions
     effect    = "Allow"
-    actions   = ["iam:GetInstanceProfile"]
+    actions   = ["iam:GetInstanceProfile", "iam:ListInstanceProfiles"]
     resources = ["*"]
   }
 }
